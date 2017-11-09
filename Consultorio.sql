96d9ea635c035e6b490ddca02a3331419dc2ed78
Rayana <rayana.azus@gmail.com>
2017-11-09 16:37:46 -0300

    Update script Consultorio

--- Consultorio.sql
+++ Consultorio.sql
@@ -46,11 +46,11 @@ create table Funcionarios(
     salario float not null
 );
 
-insert into medicos (nroa, andar, capacidade) values (1, 3, 1);
-insert into medicos (nroa, andar, capacidade) values (2, 4, 2);
-insert into medicos (nroa, andar, capacidade) values (3, 5, 6);
-insert into medicos (nroa, andar, capacidade) values (4, 2, 2);
-insert into medicos (nroa, andar, capacidade) values (5, 1, 3);
+insert into ambulatorios (nroa, andar, capacidade) values (1, 3, 1);
+insert into ambulatorios (nroa, andar, capacidade) values (2, 4, 2);
+insert into ambulatorios (nroa, andar, capacidade) values (3, 5, 6);
+insert into ambulatorios (nroa, andar, capacidade) values (4, 2, 2);
+insert into ambulatorios (nroa, andar, capacidade) values (5, 1, 3);
 
 insert into medicos (codm, cpf, nome, dataDeNascimento, cidade, especialidade, id_ambulatorio) values (1, '12345678901', 'Luis Paulo', '1989-02-10 00:00:00', 'Campina Grande', 'Cardiologista', 1);
 insert into medicos (codm, cpf, nome, dataDeNascimento, cidade, especialidade, id_ambulatorio) values (2, '09876543127', 'Elvira a rainha do deserto', '1739-02-10 00:00:00', 'Transilvania', 'Clinica geral', 4);
@@ -60,5 +60,18 @@ insert into medicos (codm, cpf, nome, dataDeNascimento, cidade, especialidade, i
 
 insert into pacientes (codp, cpf, nome, dataDeNascimento, cidade, doenca) values (1, '12345678902', 'Dona Florinda', '1739-02-10 00:00:00', 'Campina Grande', 'Chatisse');
 insert into pacientes (codp, cpf, nome, dataDeNascimento, cidade, doenca) values (2, '09875612345', 'Kiko', '1739-02-10 00:00:00', 'Campina Grande', 'Chatisse');
+insert into pacientes (codp, cpf, nome, dataDeNascimento, cidade, doenca) values (3, '97465102934', 'Nhonho', '1983-02-10 00:00:00', 'Lagoa Seca', 'Pressão alta');
+insert into pacientes (codp, cpf, nome, dataDeNascimento, cidade, doenca) values (4, '23456178909', 'Mafalda', '1999-02-10 00:00:00', 'Cabaceiras', 'Lombriga');
+insert into pacientes (codp, cpf, nome, dataDeNascimento, cidade, doenca) values (5, '12309576943', 'Faustão', '1500-02-10 00:00:00', 'Janeiro do Rio', 'Gripe');
 
-insert into Funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (1, "12345678901", 'Ana Maria Braga', '1989-02-10 00:00:00', 'Campina Grande', 1.500)
+insert into consultas (data, hora) values ('1989-02-10', '12:15:20');
+insert into consultas (data, hora) values ('1990-05-12', '17:15:20');
+insert into consultas (data, hora) values ('1996-07-10', '08:30:01');
+insert into consultas (data, hora) values ('1988-09-04', '10:10:20');
+insert into consultas (data, hora) values ('1989-02-10', '12:15:20');
+
+insert into funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (1, "12345678901", 'Ana Maria Braga', '1989-02-10 00:00:00', 'Campina Grande', 1.500);
+insert into funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (2, "12345678901", 'Luciano Hulk', '1989-02-10 00:00:00', 'Campina Grande', 1.100);
+insert into funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (3, "12345678901", 'Galvão Bueno', '1989-02-10 00:00:00', 'Campina Grande', 700);
+insert into funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (4, "12345678901", 'Ivete Sangalo', '1989-02-10 00:00:00', 'Campina Grande', 1000000000);
+insert into funcionarios (codf, cpf, nome, dataDeNascimento, cidade, salario) values (5, "12345678901", 'EU', '1989-02-10 00:00:00', 'Campina Grande', 0.0);
